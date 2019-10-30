bc3a35c45152e8dd6c4e1a1509d42b77ea6ea72e
Cristian Penarrieta <cristian.penarrieta@gusto.com>
2019-10-29 21:55:18 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const incrementAsync = () => setTimeout(() => setCount(count + 1), 100);
+  const decrementAsync = () => setTimeout(() => setCount(count - 1), 100);
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Learning react-testing-library</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p data-testid="counter-test-id">Current counter: {count}</p>
+        <div className="flex-row">
+          <button className="main-button" onClick={increment}>
+            +
+          </button>
+          <button className="main-button" onClick={decrement}>
+            -
+          </button>
+        </div>
+        <div className="flex-row">
+          <button className="main-button" onClick={incrementAsync}>
+            + Async
+          </button>
+          <button className="main-button" onClick={decrementAsync}>
+            - Async
+          </button>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  const { getByText, getByTestId, asFragment } = render(<App />);
+
+  expect(asFragment()).toMatchSnapshot();
+  expect(getByText("Learning react-testing-library")).toBeInTheDocument();
+  expect(getByTestId("counter-test-id")).toHaveTextContent("0");
+});
+
+// Fire Events
+
+it("increment counter correctly", () => {
+  const { getByText, getByTestId } = render(<App />);
+
+  fireEvent.click(getByText("+"));
+
+  expect(getByTestId("counter-test-id")).toHaveTextContent("1");
+});
+
+// fireEvent can also fill form and much more
+// fireEvent.change(getByLabelText(/username/i), {target: {value: 'chuck'}})
+// fireEvent.change(getByLabelText(/password/i), {target: {value: 'norris'}})
+
+it("decrease counter correctly", () => {
+  const { getByText, getByTestId } = render(<App />);
+
+  fireEvent.click(getByText("-"));
+
+  expect(getByTestId("counter-test-id")).toHaveTextContent("-1");
+});
+
+it("increment counter multiple times", () => {
+  const { getByText, getByTestId } = render(<App />);
+
+  const plusButton = getByText("+");
+  fireEvent.click(plusButton);
+  fireEvent.click(plusButton);
+  fireEvent.click(plusButton);
+
+  expect(getByTestId("counter-test-id")).toHaveTextContent("3");
+});
+
+it("increment/decrement counter multiple times", () => {
+  const { getByText, getByTestId } = render(<App />);
+
+  const plusButton = getByText("+");
+  const minusButton = getByText("-");
+  fireEvent.click(plusButton);
+  fireEvent.click(minusButton);
+  fireEvent.click(plusButton);
+  fireEvent.click(plusButton);
+  fireEvent.click(minusButton);
+
+  expect(getByTestId("counter-test-id")).toHaveTextContent("1");
+});
+
+// Async Events
+
+// this test fails
+xit("increment async counter correctly - wrong test", () => {
+  const { getByText, getByTestId } = render(<App />);
+
+  fireEvent.click(getByText("+ Async"));
+
+  expect(getByTestId("counter-test-id")).toHaveTextContent("1");
+});
+
+it("increment async counter correctly", async () => {
+  const { getByText } = render(<App />);
+
+  fireEvent.click(getByText("+ Async"));
+
+  await waitForElement(() => getByText("Current counter: 1"));
+});
+
+it("decrement async counter correctly", async () => {
+  const { getByText } = render(<App />);
+
+  fireEvent.click(getByText("- Async"));
+
+  await waitForElement(() => getByText("Current counter: -1"));
 });
